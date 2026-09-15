bf7b1873322875e0705c8bfac597eff953b7b167
Abhijit Moni <moniabhijit773@gmail.com>
2026-09-16 00:22:16 +0530

    largest among three num

---
+++ maximum_of_three_num.py
@@ -0,0 +1,7 @@
+def max_three_num(n1,n2,n3):
+    max(n1,n2,n3)
+    return max
+num1=int(input("enter first num: "))
+num2=int(input("enter second num: "))
+num3=int(input("enter third num: "))
+print(f"largest num is: {max_three_num(num1,num2,num3)}")   
